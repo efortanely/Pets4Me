109928c8a2091ada8970bcaff9cb90f1b45af423
Connor Sheehan <connor6321@gmail.com>
2020-03-26 22:33:16 +0000

    Resolve "Mocha can't run .ts files"

--- frontend/src/test/About.spec.ts
+++ frontend/src/test/About.spec.ts
@@ -2,7 +2,7 @@ import React from 'react'
 import { mount, shallow } from 'enzyme'
 import {expect} from 'chai'
 
-describe('About', () =>{
+describe('About', () => {
   it('should hollow world', () => {
     expect(1).to.equal(1)
   })
