29ba72afdc1139abb4992672e0c425a40e544395
deantorkelson <deantorkelson@utexas.edu>
2020-04-07 17:23:40 -0500

    added filter framework for pets

--- frontend/src/models/PetsFiltersData.ts
+++ frontend/src/models/PetsFiltersData.ts
@@ -1,19 +1,15 @@
 export interface PetsFiltersData {
     breeds: string[];
     colors: string[];
-    size_low: number;
-    size_high: number;
-    age_low: number;
-    age_high: number;
+    sizes: string[]
+    ages: string[];
     max_distance: number;
 }
 
 export let sampleFilterData = {
-    breeds: ["breed1", "breed2"],
-    colors: ["green", "fuchsia"],
-    size_low: 1,
-    size_high: 6,
-    age_low: 8,
-    age_high: 12,
-    max_distance: 40
+    breeds: ["Maltese", "Poodle"],
+    colors: ["Green", "Fuchsia"],
+    sizes: ["Small", "Medium", "Large"],
+    ages: ["Baby", "Puppy", "Adult"],
+    max_distance: 20
   };
