6c753bbeb88b520a98c82831469d39c45173647c
Cristian Garza <37551142+CristianGarza@users.noreply.github.com>
2020-04-09 11:46:58 -0500

    filters working

--- frontend/src/model-homepages/Dogs/DogsFilters.tsx
+++ frontend/src/model-homepages/Dogs/DogsFilters.tsx
@@ -1,75 +1,81 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
+import Select from 'react-select';
+import Button from 'react-bootstrap/Button'
 import Slider from '@material-ui/core/Slider'
+import { DogsFiltersData } from '../../models/DogsFiltersData'
 import { ThemeProvider } from '@material-ui/core';
-import { sliderTheme } from '../ModelHomepageUtils'
+import { sliderTheme, SelectItem, selectifyDataArray } from '../ModelHomepageUtils'
 
-function BreedsFilters() {
+interface DogsFiltersState {
+  nameInitials: string | undefined;
+  breedGroup: string | undefined;
+  maxHeight: number;
+  maxWeight: number;
+  lifespanMin: number;
+  lifespanMax: number;
+}
+
+export class DogsFilters extends React.Component<DogsFiltersData, DogsFiltersState> {
+
+    public genderData = [
+        {
+            label: "Female",
+            value: "Female"
+        },
+        {
+            label: "Male",
+            value: "Male"
+        }]
+        public nameInitials: SelectItem[] = [];
+        public breedGroup: SelectItem[] = [];
+
+    constructor(props: DogsFiltersData) {
+        super(props);
+        selectifyDataArray(this.props.name_initials, this.nameInitials);
+        selectifyDataArray(this.props.breed_group, this.breedGroup);
+        this.state = {
+            nameInitials: undefined,
+            breedGroup: undefined,
+            maxHeight: 100,
+            maxWeight: 100,
+            lifespanMin: 100,
+            lifespanMax: 100
+        } as DogsFiltersState;
+    }
+
+render() {
     return (
         <div className='filters'>
-            {/* I put dummy data in here for the purpose */}
-            <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic">
-                    Bred for
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item>Any</Dropdown.Item>
-                    <Dropdown.Item>Eat cheese</Dropdown.Item>
-                    <Dropdown.Item>Fight rats</Dropdown.Item>
-                    <Dropdown.Item>Wear sunglasses</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
 
-            <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic">
-                    Breed group
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item>Any</Dropdown.Item>
-                    <Dropdown.Item>Very small</Dropdown.Item>
-                    <Dropdown.Item>VERY big</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic">
-                    Name
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item>Affenspinscher</Dropdown.Item>
-                    <Dropdown.Item>Afghan Hound</Dropdown.Item>
-                    <Dropdown.Item>African Hunting Dog</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+            <Select options={this.nameInitials} placeholder="Select a letter..." isClearable={true}
+                onChange={(value: any) => this.setState({nameInitials: value?.value})} />
+            <Select options={this.breedGroup} placeholder="Select a breed group..." isClearable={true}
+                onChange={(value: any) => this.setState({breedGroup: value?.value})} />
 
             <ThemeProvider theme={sliderTheme}>
-                <div>
-                    <h5>Lifespan</h5>
-                    <Slider
-                        defaultValue={[2,10]}
-                        max={50}
-                        valueLabelDisplay='auto'
-                    />
-                </div>
-
-                <div>
-                    <h5>Height</h5>
-                    <Slider
-                        defaultValue={[15,50]}
-                        max={200}
-                        valueLabelDisplay='auto'
-                    />
-                </div>
-
-                <div>
-                    <h5>Weight</h5>
-                    <Slider
-                        defaultValue={[5,40]}
-                        max={200}
-                        valueLabelDisplay='auto'
-                    />
-                </div>
+                <h5>Height</h5>
+                <Slider
+                    defaultValue={0} max={this.props.max_height} valueLabelDisplay='auto'
+                    onChange={(event: any, value: any) => this.setState({maxHeight: value})}
+                />
             </ThemeProvider>
+            <ThemeProvider theme={sliderTheme}>
+                <h5>Weight</h5>
+                <Slider
+                    defaultValue={0} max={this.props.max_weight} valueLabelDisplay='auto'
+                    onChange={(event: any, value: any) => this.setState({maxWeight: value})}
+                />
+            </ThemeProvider>
+            <ThemeProvider theme={sliderTheme}>
+                <h5>Lifespan</h5>
+                <Slider
+                    defaultValue={0} min={this.props.lifespan_min} max={this.props.lifespan_max} valueLabelDisplay='auto'
+                    onChange={(event: any, value: any) => this.setState({lifespanMax: value})}
+                />
+            </ThemeProvider>
+
+            <Button variant='primary' onClick={() => console.log("current filters:: ", this.state)}>Submit</Button>
         </div>
     );
-} export default BreedsFilters;
+}
+} export default DogsFilters;
---
+++ frontend/src/models/DogsFiltersData.ts
@@ -0,0 +1,17 @@
+export interface DogsFiltersData {
+    name_initials: string[];
+    breed_group: string[];
+    max_height: number;
+    max_weight: number;
+    lifespan_min: number;
+    lifespan_max: number;
+}
+
+export let sampleFilterData = {
+    name_initials: ["D", "O", "G"],
+    breed_group: ["fun", "funner", "funnest"],
+    max_height: 4,
+    max_weight: 20,
+    lifespan_min: 6,
+    lifespan_max: 9
+} as DogsFiltersData;
