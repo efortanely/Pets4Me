40faf7ba38cd911709773af8aa8157d9c0b90204
Dean Torkelson <deantork45@gmail.com>
2020-04-26 20:58:11 +0000

    Update frontend pet age/size sorting

--- frontend/src/test/model-homepages/Query/PetFilters.spec.tsx
+++ frontend/src/test/model-homepages/Query/PetFilters.spec.tsx
@@ -18,7 +18,7 @@ describe('<PetsFilters />', () => {
       size: [],
       age: [],
       distanceMax: 1000,
-      sortType: undefined,
+      sortType: "",
       sortDir: "desc"
   }
 
@@ -30,4 +30,24 @@ describe('<PetsFilters />', () => {
   it('default call to the submit button with no options selected', () => {
     expect(constructQuery(testDefaultFilterState)).to.equal("zip_code=78705&max_dist=1000")
   });
+
+  // author Dean
+  it('should put size filter in separate url param', () => {
+    let sizeFilterAsc = testDefaultFilterState;
+    sizeFilterAsc.sortDir = "asc";
+    sizeFilterAsc.sortType = "size";
+    let query = constructQuery(sizeFilterAsc); 
+    expect(query).to.contain("sort=size&dir=asc")
+    expect(query).not.to.contain("order_by")
+  })
+
+  // author Dean
+  it('should put age filter in separate url param', () => {
+    let ageFilterDesc = testDefaultFilterState;
+    ageFilterDesc.sortDir = "desc";
+    ageFilterDesc.sortType = "age";
+    let query = constructQuery(ageFilterDesc); 
+    expect(query).to.contain("sort=age&dir=desc")
+    expect(query).not.to.contain("order_by")
+  })
 })
